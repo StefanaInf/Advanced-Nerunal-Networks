f982dee9a5664a9a3af8b69d9f2b5848955d0fba
Biceada Stefana <91457306+StefanaInf@users.noreply.github.com>
2024-10-21 19:56:42 +0300

    Update Version 2.py

--- Version 2.py
+++ Version 2.py
@@ -33,7 +33,7 @@ optimized_transforms = v2.Compose([
     v2.CenterCrop(32),
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), inplace=True)
+    transforms.Normalize((0.5, 0.5, 0.5), (0.25, 0.25, 0.25))
 ])
 
 train_set = CIFAR100('/kaggle/input/fii-atnn-2024-assignment-2', download=False, train=True,
